152265ab782a7d3fda1a0134bda39e37fe284b3e
Franco <fmaurizzio@alu.ing.unlp.edu.ar>
2020-11-26 00:24:15 -0300

    Agregado index de heroes,items,enemies

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\HeroController;
+use App\Http\Controllers\ItemController;
+use App\Http\Controllers\EnemyController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +20,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Route::get('/admin', 'App\Http\Controllers\AdminController@index');
+Route::get('/admin', [AdminController::class, 'index'])->name('admin');
+Route::get('/admin/heroes', [HeroController::class, 'index'])->name('admin.heroes');
+Route::get('/admin/items', [ItemController::class, 'index'])->name('admin.items');
+Route::get('/admin/enemies', [EnemyController::class, 'index'])->name('admin.enemies');
+
+
+
+
+
+?>
